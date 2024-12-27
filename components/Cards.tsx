872941cda211cd7b8ffbb723d0b523e6f95d204f
Jakob Johnson <jakob.johnson.dev@gmail.com>
2024-12-27 19:49:02 +1100

    Database added and backend complete

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,19 +2,24 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 
 import icons from '@/constants/icons';
 import images from '@/constants/images';
+import { Models } from 'react-native-appwrite';
 
-interface Props {
+interface CardProps {
   onPress?: () => void;
+  item: Models.Document;
 }
 
-export function FeaturedCard({ onPress }: Props) {
+export function FeaturedCard({
+  onPress,
+  item: { image, rating, name, address, price },
+}: CardProps) {
   return (
     <TouchableOpacity
       onPress={onPress}
       className='flex flex-col items-start w-60 h-80 relative'
     >
       <Image
-        source={images.japan}
+        source={{ uri: image }}
         className='size-full rounded-2xl'
       />
       <Image
@@ -28,7 +33,7 @@ export function FeaturedCard({ onPress }: Props) {
           className='size-3.5'
         />
         <Text className='text-xs font-rubik-bold text-primary-300 ml-1'>
-          4.4
+          {rating}
         </Text>
       </View>
 
@@ -37,14 +42,12 @@ export function FeaturedCard({ onPress }: Props) {
           className='text-xl font-rubik-bold text-white'
           numberOfLines={1}
         >
-          Modern Apartment
+          {name}
         </Text>
-        <Text className='text-base font-rubik text-white'>
-          22 W 15th St, New York, NY 10011
-        </Text>
+        <Text className='text-base font-rubik text-white'>{address}</Text>
 
         <View className='flex flex-row items-center justify-between w-full'>
-          <Text className='text-xl font-rubik-bold text-white'>$2,500</Text>
+          <Text className='text-xl font-rubik-bold text-white'>${price}</Text>
           <Image
             source={icons.heart}
             className='size-5'
@@ -55,10 +58,45 @@ export function FeaturedCard({ onPress }: Props) {
   );
 }
 
-export function Card() {
+export function Card({
+  onPress,
+  item: { image, rating, name, address, price },
+}: CardProps) {
   return (
-    <View>
-      <Text>Card</Text>
-    </View>
+    <TouchableOpacity
+      onPress={onPress}
+      className='flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative'
+    >
+      <View className='flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50'>
+        <Image
+          source={icons.star}
+          className='size-2.5'
+        />
+        <Text className='text-xs font-rubik-bold text-primary-300 ml-0.5'>
+          {rating}
+        </Text>
+      </View>
+
+      <Image
+        source={{ uri: image }}
+        className='w-full h-40 rounded-lg'
+      />
+
+      <View className='flex flex-col mt-2'>
+        <Text className='text-base font-rubik-bold text-black-300'>{name}</Text>
+        <Text className='text-xs font-rubik text-black-200'>{address}</Text>
+
+        <View className='flex flex-row items-center justify-between mt-2'>
+          <Text className='text-base font-rubik-bold text-primary-300'>
+            ${price}
+          </Text>
+          <Image
+            source={icons.heart}
+            className='w-5 h-5 mr-2'
+            tintColor='#191d31'
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 }
