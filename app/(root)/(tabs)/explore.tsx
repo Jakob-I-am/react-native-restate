4fe52608e66cc31a5afaf81ca84c2bfb5f64aba2
jakob-i-am <0xjakob@gmail.com>
2024-12-30 19:56:47 +1100

    started explore page

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,10 +1,104 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { router, useLocalSearchParams } from 'expo-router';
+import { useEffect } from 'react';
+
+import icons from '@/constants/icons';
+import Search from '@/components/Search';
+import { Card, FeaturedCard } from '@/components/Cards';
+import Filters from '@/components/Filters';
+import { useAppwrite } from '@/lib/useAppwrite';
+import { getProperties } from '@/lib/appwrite';
+import NoResults from '@/components/NoResults';
 
 export default function Explore() {
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+  const {
+    data: properties,
+    loading: propertiesLoading,
+    refetch,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 20,
+    },
+    skip: true,
+  });
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+  useEffect(() => {
+    refetch({ filter: params.filter!, query: params.query!, limit: 20 });
+  }, [params.filter, params.query]);
+
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <FlatList
+        data={properties}
+        renderItem={({ item }) => (
+          <Card
+            onPress={() => handleCardPress(item.$id)}
+            item={item}
+          />
+        )}
+        keyExtractor={(item) => item.$id}
+        numColumns={2}
+        contentContainerClassName='pb-32'
+        columnWrapperClassName='flex gap-5 px-5'
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          propertiesLoading ? (
+            <ActivityIndicator
+              className='text-primary-300 mt-5'
+              size='large'
+            />
+          ) : (
+            <NoResults />
+          )
+        }
+        ListHeaderComponent={
+          <View className='px-5'>
+            <View className='flex flex-row items-center justify-between mt-5'>
+              <TouchableOpacity
+                className='flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center'
+                onPress={() => router.back()}
+              >
+                <Image
+                  source={icons.backArrow}
+                  className='size-5'
+                />
+              </TouchableOpacity>
+
+              <Text className='text-base mr-2 text-center font-rubik-medium text-black-300'>
+                Search for Your Ideal Home
+              </Text>
+              <Image
+                source={icons.bell}
+                className='w-6 h-6'
+              />
+            </View>
+            <Search />
+
+            <View className='mt-5'>
+              <Filters />
+
+              <Text className='text-xl font-rubik-bold text-black-100 mt-5'>
+                Found {properties?.length} properties
+              </Text>
+            </View>
+          </View>
+        }
+      />
+    </SafeAreaView>
   );
 }
--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,13 +1,126 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { router, useLocalSearchParams } from 'expo-router';
+
+import { useAppwrite } from '@/lib/useAppwrite';
+import { getPropertyById } from '@/lib/appwrite';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import icons from '../../../constants/icons';
 
 export default function Property() {
   const { id } = useLocalSearchParams();
+  const { data, loading } = useAppwrite({
+    fn: getPropertyById,
+    params: {
+      id: id.toString(),
+    },
+  });
 
   return (
-    <View>
-      <Text>Property {id}</Text>
-    </View>
+    <SafeAreaView>
+      <View className='flex flex-row items-start justify-between px-6 w-full h-80'>
+        <TouchableOpacity
+          className=''
+          onPress={() => router.back()}
+        >
+          <Image
+            source={icons.backArrow}
+            className='size-8'
+          />
+        </TouchableOpacity>
+        <View className='flex flex-row items-center'>
+          <TouchableOpacity>
+            <Image
+              source={icons.heart}
+              className='size-8'
+              tintColor={'#191d31'}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image
+              source={icons.send}
+              className='size-8 ml-5'
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View className='flex gap-y-5 px-6'>
+        <Text className='text-2xl font-rubik-bold text-black-300'>
+          {data?.name}
+        </Text>
+        <View className='flex flex-row items-center px-2'>
+          <Text className='text-xs uppercase text-primary-300 font-rubik-bold mr-6'>
+            {data?.type}
+          </Text>
+          <View className='flex flex-row items-center justify-between'>
+            <Image source={icons.star} />
+            <Text className='ml-2 text-sm text-black-300 font-rubik'>
+              {data?.rating} <Text>({data?.reviews?.length} reviews)</Text>
+            </Text>
+          </View>
+        </View>
+
+        <View className='flex flex-row items-center justify-between px-8'>
+          <View className='flex flex-row items-center'>
+            <Image
+              source={icons.bed}
+              className='size-5 mr-4'
+            />
+            <Text className='text-sm text-black-300 font-rubik-bold'>
+              {data?.bedrooms} beds
+            </Text>
+          </View>
+          <View className='flex flex-row items-center justify-center'>
+            <Image
+              source={icons.bath}
+              className='size-5 mr-4'
+            />
+            <Text className='text-sm text-black-300 font-rubik-bold'>
+              {data?.bathrooms} bath
+            </Text>
+          </View>
+          <View className='flex flex-row items-center justify-center'>
+            <Image
+              source={icons.area}
+              className='size-5 mr-4'
+            />
+            <Text className='text-sm text-black-300 font-rubik-bold'>
+              {data?.area} sqft
+            </Text>
+          </View>
+        </View>
+
+        <View className='flex mt-10 gap-y-5'>
+          <Text className='mb-2 text-xl font-rubik-bold text-black-300'>
+            Agent
+          </Text>
+          <View className='flex flex-row items-center justify-between'>
+            <View>
+              <Image source={{ uri: data?.agent?.avatar }} />
+              <Text>{data?.agent?.name}</Text>
+            </View>
+          </View>
+
+          <Text className='mb-2 text-xl font-rubik-bold text-black-300'>
+            Overview
+          </Text>
+          <Text>{data?.description}</Text>
+
+          <Text className='mb-2 text-xl font-rubik-bold text-black-300'>
+            Facilities
+          </Text>
+          <View className='flex flex-row '>
+            {data?.facilities.map((item: string, index: number) => (
+              <Text
+                className=''
+                key={index}
+              >
+                {item}
+              </Text>
+            ))}
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
